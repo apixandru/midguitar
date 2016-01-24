a7d550e760455ecf4b3cebd3b3bcb975ea715878
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-01-24 12:11:47 +0200

    put notes on the panel

--- midguitar-model/src/main/java/com/apixandru/midguitar/model/BaseNote.java
+++ midguitar-model/src/main/java/com/apixandru/midguitar/model/BaseNote.java
@@ -6,27 +6,63 @@ package com.apixandru.midguitar.model;
  */
 public enum BaseNote {
 
-    C("C"), CS("C#"),
-    D("D"), DS("D#"),
+    C("C"), CS(C),
+    D("D"), DS(D),
     E("E"),
-    F("F"), FS("F#"),
-    G("G"), GS("G#"),
-    A("A"), AS("A#"),
+    F("F"), FS(F),
+    G("G"), GS(G),
+    A("A"), AS(A),
     B("B");
 
     private final String value;
+    private final boolean sharp;
+    private final BaseNote baseNote;
+
+    /**
+     * @param value the actual note
+     */
+    BaseNote(final BaseNote value) {
+        this(null, value, true);
+    }
 
     /**
      * @param value the value of the note
      */
     BaseNote(final String value) {
-        this.value = value;
+        this(value, null, false);
+    }
+
+    /**
+     * @param value the value of the note
+     * @param sharp
+     */
+    BaseNote(final String value, final BaseNote baseNote, final boolean sharp) {
+        this.sharp = sharp;
+        this.baseNote = baseNote == null ? this : baseNote;
+        this.value = baseNote == null ? value : baseNote.value;
+    }
+
+    /**
+     * @return sharp
+     */
+    public boolean isSharp() {
+        return sharp;
+    }
+
+    /**
+     * @return
+     */
+    public BaseNote getBaseNote() {
+        return baseNote;
     }
 
     /**
      * @return the value of the note
      */
     public String getValue() {
+        if (isSharp()) {
+            return value + '#';
+        }
         return value;
     }
 
--- midguitar-swing/src/main/java/com/apixandru/midguitar/swing/MidguitarPanel.java
+++ midguitar-swing/src/main/java/com/apixandru/midguitar/swing/MidguitarPanel.java
@@ -1,8 +1,15 @@
 package com.apixandru.midguitar.swing;
 
+import com.apixandru.midguitar.model.BaseNote;
+import com.apixandru.midguitar.model.Notes;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Alexandru-Constantin Bledea
@@ -10,16 +17,24 @@ import java.awt.*;
  */
 public class MidguitarPanel extends JPanel {
 
-    private static final Font FONT_NOTE = new Font(null, Font.PLAIN, 136);
-    private static final String WHOLE_NOTE_UNICODE = "\uD834\uDD5D";
+    private static final int DISTANCE_BETWEEN_LINES = 30;
+    private static final int DISTANCE_SEMITONE = DISTANCE_BETWEEN_LINES / 2;
+    private static final int DISTANCE_BETWEEN_NOTES = 70;
 
-    private static final int DISTANCE_BETWEEN_NOTES = 30;
+    private static final int POS_E2 = 420;
+    private static final int NOTE_E2 = 40;
 
     private final int fromX;
     private final int toX;
 
-    private static final int START_Y = 20;
+    private static final int START_Y = 35;
 
+    private BufferedImage imgClefG;
+    private BufferedImage imgWholeNote;
+    private BufferedImage imgModSharp;
+
+    private Integer noteExpected = 58;
+    private Integer noteActual = 64;
 
     MidguitarPanel() {
         setBorder(new LineBorder(Color.BLACK, 2));
@@ -28,28 +43,87 @@ public class MidguitarPanel extends JPanel {
         fromX = 20;
         toX = width - fromX;
 
-        final Dimension minimumSize = new Dimension(width, 430);
+        setBackground(Color.WHITE);
+        setOpaque(true);
+
+        final Dimension minimumSize = new Dimension(width, 460);
         setMinimumSize(minimumSize);
         setPreferredSize(minimumSize);
+        loadImages();
     }
 
     @Override
     public void paint(final Graphics g) {
         super.paint(g);
+
+
+        drawNote(g, noteExpected, false);
+        drawNote(g, noteActual, true);
+
+        drawLines((Graphics2D) g);
+        g.drawImage(imgClefG, 20, 183, null);
+
+    }
+
+    private void drawNote(Graphics g, final Integer note, final boolean actual) {
+        if (null == note) {
+            return;
+        }
+        int x = 120;
+        if (actual) {
+            x += DISTANCE_BETWEEN_NOTES;
+        }
+
+        int y = POS_E2 - getFullNotesInBetween(note) * DISTANCE_SEMITONE;
+        g.drawImage(imgWholeNote, x, y, null);
+        boolean isSharp = Notes.getBaseNote(note).isSharp();
+        if (isSharp) {
+            g.drawImage(imgModSharp, x - 29, y - 14, null);
+        }
+    }
+
+    private int getFullNotesInBetween(final int note) {
+        int count = 0;
+        BaseNote lastNote = BaseNote.E;
+        for (int i = NOTE_E2; i <= note; i++) {
+            final BaseNote currentNote = Notes.getBaseNote(i);
+            if (currentNote.getBaseNote() != lastNote) {
+                count++;
+            }
+            lastNote = currentNote.getBaseNote();
+        }
+        return count;
+    }
+
+    /**
+     * @param g
+     */
+    private void drawLines(final Graphics2D g) {
         final Color originalColor = g.getColor();
-        final Color outOfRangeColor = Color.red;
+        final Color outOfRangeColor = new Color(0x11000000, true);
         g.setColor(outOfRangeColor);
-        Graphics2D g2 = (Graphics2D) g;
-        final Stroke originalStroke = ((Graphics2D) g).getStroke();
-        g2.setStroke(new BasicStroke(2));
+        final Stroke originalStroke = g.getStroke();
+        g.setStroke(new BasicStroke(4f));
         drawLines(6, 0, g);
-        g.setColor(originalColor);
+        g.setColor(Color.BLACK);
         drawLines(5, 6, g);
         g.setColor(outOfRangeColor);
         drawLines(3, 11, g);
         g.setColor(originalColor);
-        g2.setStroke(originalStroke);
+        g.setStroke(originalStroke);
+    }
 
+
+    private void loadImages() {
+        try (final InputStream streamCg = MidguitarFrame.class.getResourceAsStream("/clef_g.png");
+             final InputStream streamNw = MidguitarFrame.class.getResourceAsStream("/note_whole.png");
+             final InputStream streamMs = MidguitarFrame.class.getResourceAsStream("/mod_sharp.png")) {
+            imgClefG = ImageIO.read(streamCg);
+            imgWholeNote = ImageIO.read(streamNw);
+            imgModSharp = ImageIO.read(streamMs);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     /**
@@ -59,8 +133,9 @@ public class MidguitarPanel extends JPanel {
      */
     private void drawLines(final int howMany, final int startingFrom, final Graphics g) {
         for (int i = startingFrom, to = startingFrom + howMany; i < to; i++) {
-            final int y = START_Y + i * DISTANCE_BETWEEN_NOTES;
+            final int y = START_Y + i * DISTANCE_BETWEEN_LINES;
             g.drawLine(fromX, y, toX, y);
         }
     }
+
 }
