9643e05a4cced835fbf6f0033377b05e3c29e854
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-01-29 23:30:54 +0200

    sketch settings panel

---
+++ midguitar-swing/src/main/java/com/apixandru/midguitar/swing/MidguitarSettings.java
@@ -0,0 +1,110 @@
+package com.apixandru.midguitar.swing;
+
+import com.apixandru.midguitar.model.MidiDevices;
+
+import javax.sound.midi.MidiDevice;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since January 29, 2016
+ */
+public class MidguitarSettings extends JPanel {
+
+    private final DefaultComboBoxModel<MidiDevice> modelInput = new DefaultComboBoxModel<>();
+    private final DefaultComboBoxModel<MidiDevice> modelOuput = new DefaultComboBoxModel<>();
+
+    MidguitarSettings(final MidiDevices deviceProvider) {
+        final Dimension minimumSize = new Dimension(500, 460);
+        setMinimumSize(minimumSize);
+        setPreferredSize(minimumSize);
+        setBorder(new LineBorder(Color.BLACK, 2));
+        setLayout(new BorderLayout());
+        final JPanel jPanel = new JPanel();
+        add(jPanel, BorderLayout.NORTH);
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+        jPanel.add(createPanel("Enable Input", modelInput, deviceProvider::getInputDevices));
+        jPanel.add(createPanel("Enable Output", modelOuput, deviceProvider::getSynthesizers));
+        jPanel.add(createConfig());
+        jPanel.add(createStart());
+    }
+
+    private Component createStart() {
+        JPanel jPanel = new JPanel();
+        jPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.X_AXIS));
+        jPanel.add(Box.createHorizontalGlue());
+        jPanel.add(new JButton("Start"));
+        return jPanel;
+    }
+
+    private JPanel createConfig() {
+        final JPanel noteControl = new JPanel();
+        noteControl.setLayout(new BoxLayout(noteControl, BoxLayout.X_AXIS));
+        final JCheckBox chk = new JCheckBox();
+        chk.setText("Include sharp notes");
+        noteControl.add(chk);
+        noteControl.add(new JLabel("   From   "));
+        noteControl.add(new JComboBox<>());
+        noteControl.add(new JLabel("   To   "));
+        noteControl.add(new JComboBox<>());
+        noteControl.setBorder(new EmptyBorder(5, 5, 5, 5));
+        return noteControl;
+    }
+
+    /**
+     * @param enableText
+     * @param model
+     * @param deviceMethod
+     * @return
+     */
+    private static JPanel createPanel(
+            final String enableText,
+            final DefaultComboBoxModel<MidiDevice> model,
+            final Callable<List<? extends MidiDevice>> deviceMethod) {
+
+        final JPanel jPanel = new JPanel();
+        jPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.X_AXIS));
+
+        JCheckBox cbUseOutputDevice = new JCheckBox();
+        cbUseOutputDevice.setText(enableText);
+        jPanel.add(cbUseOutputDevice);
+        final JComboBox<MidiDevice> cmbOutput = new JComboBox<>(model);
+        cmbOutput.setRenderer(new MidiCellRenderer());
+//        cmbOutput.setMaximumSize(new Dimension(200, Integer.MAX_VALUE));
+//        cmbOutput.setPreferredSize(new Dimension(100, cmbOutput.getPreferredSize().height));
+
+        jPanel.add(cmbOutput);
+        jPanel.add(Box.createHorizontalStrut(5));
+        jPanel.add(new JButton("Reload"));
+        refreshSynthesizers(model, deviceMethod);
+        return jPanel;
+    }
+
+    /**
+     * @param model
+     * @param deviceProvider
+     */
+    private static void refreshSynthesizers(final DefaultComboBoxModel<MidiDevice> model, final Callable<java.util.List<? extends MidiDevice>> deviceProvider) {
+        try {
+            model.removeAllElements();
+            deviceProvider.call()
+                    .forEach(model::addElement);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Cannot reload devices",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+
+        }
+    }
+
+
+}
