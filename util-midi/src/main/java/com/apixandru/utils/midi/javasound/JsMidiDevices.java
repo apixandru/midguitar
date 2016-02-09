e04e783849426ef83d2631827a96c564fae657b9
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-02-09 07:41:05 +0200

    make jsmididevices a midiprovider

--- util-midi/src/main/java/com/apixandru/utils/midi/javasound/JsMidiDevices.java
+++ util-midi/src/main/java/com/apixandru/utils/midi/javasound/JsMidiDevices.java
@@ -1,6 +1,8 @@
 package com.apixandru.utils.midi.javasound;
 
+import com.apixandru.utils.midi.MidiException;
 import com.apixandru.utils.midi.MidiInput;
+import com.apixandru.utils.midi.MidiProvider;
 
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
@@ -15,7 +17,7 @@ import java.util.stream.Stream;
  * @author Alexandru-Constantin Bledea
  * @since January 24, 2016
  */
-public class JsMidiDevices {
+public class JsMidiDevices implements MidiProvider {
 
     /**
      * @return the midi devices
@@ -42,14 +44,18 @@ public class JsMidiDevices {
     }
 
     /**
-     * @return the input devices
-     * @throws MidiUnavailableException
+     * @return the javasound input devices
+     * @throws MidiException
      */
-    public List<MidiInput> getInputDevices() throws MidiUnavailableException {
-        return getDevices()
-                .filter(dev -> 0 != dev.getMaxTransmitters())
-                .map(JsMidiDevice::new)
-                .collect(Collectors.toList());
+    public List<MidiInput> getInputDevices() throws MidiException {
+        try {
+            return getDevices()
+                    .filter(dev -> 0 != dev.getMaxTransmitters())
+                    .map(JsMidiDevice::new)
+                    .collect(Collectors.toList());
+        } catch (final MidiUnavailableException ex) {
+            throw new MidiException("Midi devices unavailable", ex);
+        }
     }
 
 }
